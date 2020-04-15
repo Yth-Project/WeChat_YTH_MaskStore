52eebc5c7b46bb219891c56904246601face3956
BikaPika <944037821@qq.com>
2020-04-15 17:43:50 +0800

    首页

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,8 +1,20 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
+	<!-- 头部轮播 -->
+	<view class="carousel-section">
+		<!-- 标题栏和状态栏占位符 -->
+		<view class="titleNview-placing"></view>
+		<!-- 背景色区域 -->
+		<view class="titleNview-background" :style="{backgroundColor:titleNViewBackground}"></view>
+		<swiper class="carousel" circular @change="swiperChange">
+			<swiper-item v-for="(item, index) in carouselList" :key="index" class="carousel-item" @click="navToDetailPage({title: '轮播广告'})">
+				<image :src="item.src" />
+			</swiper-item>
+		</swiper>
+		<!-- 自定义swiper指示器 -->
+		<view class="swiper-dots">
+			<text class="num">{{swiperCurrent+1}}</text>
+			<text class="sign">/</text>
+			<text class="num">{{swiperLength}}</text>
 		</view>
 	</view>
 </template>
@@ -11,42 +23,38 @@
 	export default {
 		data() {
 			return {
-				title: 'Hello'
+				carouselList: [],
 			}
 		},
 		onLoad() {
 
 		},
 		methods: {
-
+			swiperChange() {},
 		}
 	}
 </script>
 
-<style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
+<style lang="scss">
+/* 头部 轮播图 */
+.carousel-section {
+	position: relative;
+	padding-top: 10px;
+
+	.titleNview-placing {
+		height: var(--status-bar-height);
+		padding-top: 44px;
+		box-sizing: content-box;
 	}
 
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
+	.titleNview-background {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 426upx;
+		transition: .4s;
 	}
+}
 
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
 </style>
