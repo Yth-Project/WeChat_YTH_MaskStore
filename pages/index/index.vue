092103e8e1e4997730933b44a2f059f4dc1ce82c
xuziyun <944037821@qq.com>
2020-04-16 15:28:47 +0800

    到商品详情

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,21 +1,25 @@
 <template>
-	<!-- 头部轮播 -->
-	<view class="carousel-section">
-		<!-- 标题栏和状态栏占位符 -->
-		<view class="titleNview-placing"></view>
-		<!-- 背景色区域 -->
-		<view class="titleNview-background" :style="{backgroundColor:titleNViewBackground}"></view>
-		<swiper class="carousel" circular @change="swiperChange">
-			<swiper-item v-for="(item, index) in carouselList" :key="index" class="carousel-item" @click="navToDetailPage({title: '轮播广告'})">
-				<image :src="item.src" />
-			</swiper-item>
-		</swiper>
-		<!-- 自定义swiper指示器 -->
-		<view class="swiper-dots">
-			<text class="num">{{swiperCurrent+1}}</text>
-			<text class="sign">/</text>
-			<text class="num">{{swiperLength}}</text>
+	<view class="container">
+		<!-- 头部轮播 -->
+		<view class="carousel-section">
+			<swiper class="carousel" @change="swiperChange" autoplay interval="2000" duration="500">
+				<swiper-item v-for="(item, index) in carouselList" :key="index" class="carousel-item">
+					<image :src="item.src" @click="navToDetailPage(item.id)" mode="aspectFill" />
+				</swiper-item>
+			</swiper>
+			<!-- 重置小圆点的样式 -->
+			<view class="dots">
+				<block v-for="(item, index) in carouselList" :key="index">
+					<view :class="index == currentSwiper ? 'dot active' : 'dot'"></view>
+				</block>
+			</view>
 		</view>
+		<block v-for="(item, index) in productList" :key="index">
+			<view class="mask-name">{{item.title}}</view>
+			<view class="mask-img">
+				<image :src="item.src" mode="aspectFill"></image>
+			</view>
+		</block>
 	</view>
 </template>
 
@@ -23,38 +27,134 @@
 	export default {
 		data() {
 			return {
-				carouselList: [],
+				carouselList: [{
+						id: 0,
+						src: '/static/logo.png',
+						backgroundColor: 'rgba(203,87,60)'
+					},
+					{
+						id: 1,
+						src: '/static/logo.png',
+						backgroundColor: 'rgba(205,216,218)'
+					},
+					{
+						id: 2,
+						src: '/static/logo.png',
+						backgroundColor: 'rgba(183,73,68)'
+					},
+				],
+				currentSwiper: 0,
+				productList: [{
+						id: 0,
+						title: '医用外科口罩',
+						src: '/static/logo.png'
+					},
+					{
+						id: 1,
+						title: '一次性医用口罩',
+						src: '/static/logo.png'
+					},
+					{
+						id: 2,
+						title: 'KN95口罩',
+						src: '/static/logo.png'
+					},
+				]
 			}
 		},
 		onLoad() {
 
 		},
+		onShow() {
+			
+		},
 		methods: {
-			swiperChange() {},
+			swiperChange(e) {
+				this.currentSwiper = e.detail.current
+			},
+			navToDetailPage(id) {
+				console.log(id)
+				uni.navigateTo({
+					url: `/pages/product/product?id=${id}`
+				})
+			},
 		}
 	}
 </script>
 
 <style lang="scss">
-/* 头部 轮播图 */
-.carousel-section {
-	position: relative;
-	padding-top: 10px;
+	page {
+		.carousel-section {
+			padding: 0;
+			position: relative;
 
-	.titleNview-placing {
-		height: var(--status-bar-height);
-		padding-top: 44px;
-		box-sizing: content-box;
+			.carousel {
+				width: 100%;
+				height: 600upx;
+
+				.carousel-item {
+					width: 100%;
+					height: 600upx;
+
+					image {
+						width: 100%;
+						height: 100%;
+					}
+				}
+			}
+
+			/*用来包裹所有的小圆点 */
+			.dots {
+				height: 12upx;
+				display: flex;
+				flex-direction: row;
+				justify-content: space-between;
+				position: absolute;
+				bottom: 20upx;
+				left: 50%;
+				transform: translateX(-50%);
+			}
+
+			/*未选中时的小圆点样式 */
+			.dot {
+				width: 12upx;
+				height: 12upx;
+				border-radius: 6upx;
+				background: #599590;
+				margin-right: 18upx;
+			}
+
+			.dot:last-child {
+				margin-right: 0;
+			}
+
+			/*选中以后的小圆点样式 */
+			.active {
+				width: 39upx;
+				height: 12upx;
+				border-radius: 6upx;
+				background-color: #55DACE;
+			}
+		}
+
+		// 商品列表title
+		.mask-name {
+			height: 90upx;
+			width: 712upx;
+			padding-left: 38upx;
+			line-height: 90upx;
+			color: #333333;
+			font-size: 30upx;
+		}
+
+		.mask-img {
+			width: 100%;
+			height: 658upx;
+
+			image {
+				width: 100%;
+				height: 658upx;
+			}
+		}
 	}
-
-	.titleNview-background {
-		position: absolute;
-		top: 0;
-		left: 0;
-		width: 100%;
-		height: 426upx;
-		transition: .4s;
-	}
-}
-
 </style>
--- unpackage/dist/dev/mp-weixin/common/main.js
+++ unpackage/dist/dev/mp-weixin/common/main.js
@@ -1,15 +1,16 @@
 (global["webpackJsonp"] = global["webpackJsonp"] || []).push([["common/main"],[
 /* 0 */
-/*!**************************************************************!*\
-  !*** C:/Users/shiun/Documents/HBuilderProjects/test/main.js ***!
-  \**************************************************************/
+/*!****************************************************************************!*\
+  !*** C:/Users/徐紫云/Documents/HBuilderProjects/WeChat_YTH_MaskStore/main.js ***!
+  \****************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
 /* WEBPACK VAR INJECTION */(function(createApp) {__webpack_require__(/*! uni-pages */ 4);__webpack_require__(/*! @dcloudio/uni-stat */ 5);
 var _vue = _interopRequireDefault(__webpack_require__(/*! vue */ 2));
-var _App = _interopRequireDefault(__webpack_require__(/*! ./App */ 9));function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}function ownKeys(object, enumerableOnly) {var keys = Object.keys(object);if (Object.getOwnPropertySymbols) {var symbols = Object.getOwnPropertySymbols(object);if (enumerableOnly) symbols = symbols.filter(function (sym) {return Object.getOwnPropertyDescriptor(object, sym).enumerable;});keys.push.apply(keys, symbols);}return keys;}function _objectSpread(target) {for (var i = 1; i < arguments.length; i++) {var source = arguments[i] != null ? arguments[i] : {};if (i % 2) {ownKeys(Object(source), true).forEach(function (key) {_defineProperty(target, key, source[key]);});} else if (Object.getOwnPropertyDescriptors) {Object.defineProperties(target, Object.getOwnPropertyDescriptors(source));} else {ownKeys(Object(source)).forEach(function (key) {Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key));});}}return target;}function _defineProperty(obj, key, value) {if (key in obj) {Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true });} else {obj[key] = value;}return obj;}
+var _App = _interopRequireDefault(__webpack_require__(/*! ./App */ 9));
+var _store = _interopRequireDefault(__webpack_require__(/*! ./store */ 15));function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}function ownKeys(object, enumerableOnly) {var keys = Object.keys(object);if (Object.getOwnPropertySymbols) {var symbols = Object.getOwnPropertySymbols(object);if (enumerableOnly) symbols = symbols.filter(function (sym) {return Object.getOwnPropertyDescriptor(object, sym).enumerable;});keys.push.apply(keys, symbols);}return keys;}function _objectSpread(target) {for (var i = 1; i < arguments.length; i++) {var source = arguments[i] != null ? arguments[i] : {};if (i % 2) {ownKeys(Object(source), true).forEach(function (key) {_defineProperty(target, key, source[key]);});} else if (Object.getOwnPropertyDescriptors) {Object.defineProperties(target, Object.getOwnPropertyDescriptors(source));} else {ownKeys(Object(source)).forEach(function (key) {Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key));});}}return target;}function _defineProperty(obj, key, value) {if (key in obj) {Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true });} else {obj[key] = value;}return obj;}
 
 _vue.default.config.productionTip = false;
 
@@ -31,9 +32,9 @@ createApp(app).$mount();
 /* 7 */,
 /* 8 */,
 /* 9 */
-/*!**************************************************************!*\
-  !*** C:/Users/shiun/Documents/HBuilderProjects/test/App.vue ***!
-  \**************************************************************/
+/*!****************************************************************************!*\
+  !*** C:/Users/徐紫云/Documents/HBuilderProjects/WeChat_YTH_MaskStore/App.vue ***!
+  \****************************************************************************/
 /*! no static exports found */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
@@ -65,14 +66,14 @@ var component = Object(_Downloads_HBuilderX_2_6_11_20200409_HBuilderX_plugins_un
   renderjs
 )
 
-component.options.__file = "Documents/HBuilderProjects/test/App.vue"
+component.options.__file = "Documents/HBuilderProjects/WeChat_YTH_MaskStore/App.vue"
 /* harmony default export */ __webpack_exports__["default"] = (component.exports);
 
 /***/ }),
 /* 10 */
-/*!***************************************************************************************!*\
-  !*** C:/Users/shiun/Documents/HBuilderProjects/test/App.vue?vue&type=script&lang=js& ***!
-  \***************************************************************************************/
+/*!*****************************************************************************************************!*\
+  !*** C:/Users/徐紫云/Documents/HBuilderProjects/WeChat_YTH_MaskStore/App.vue?vue&type=script&lang=js& ***!
+  \*****************************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
@@ -85,9 +86,9 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 /* 11 */
-/*!**********************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************!*\
-  !*** ./node_modules/babel-loader/lib!./node_modules/@dcloudio/vue-cli-plugin-uni/packages/webpack-preprocess-loader??ref--12-1!./node_modules/@dcloudio/webpack-uni-mp-loader/lib/script.js!./node_modules/@dcloudio/vue-cli-plugin-uni/packages/vue-loader/lib??vue-loader-options!./node_modules/@dcloudio/webpack-uni-mp-loader/lib/style.js!C:/Users/shiun/Documents/HBuilderProjects/test/App.vue?vue&type=script&lang=js& ***!
-  \**********************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************/
+/*!************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/babel-loader/lib!./node_modules/@dcloudio/vue-cli-plugin-uni/packages/webpack-preprocess-loader??ref--12-1!./node_modules/@dcloudio/webpack-uni-mp-loader/lib/script.js!./node_modules/@dcloudio/vue-cli-plugin-uni/packages/vue-loader/lib??vue-loader-options!./node_modules/@dcloudio/webpack-uni-mp-loader/lib/style.js!C:/Users/徐紫云/Documents/HBuilderProjects/WeChat_YTH_MaskStore/App.vue?vue&type=script&lang=js& ***!
+  \************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
@@ -106,9 +107,9 @@ Object.defineProperty(exports, "__esModule", { value: true });exports.default = 
 
 /***/ }),
 /* 12 */
-/*!***********************************************************************************************!*\
-  !*** C:/Users/shiun/Documents/HBuilderProjects/test/App.vue?vue&type=style&index=0&lang=css& ***!
-  \***********************************************************************************************/
+/*!*************************************************************************************************************!*\
+  !*** C:/Users/徐紫云/Documents/HBuilderProjects/WeChat_YTH_MaskStore/App.vue?vue&type=style&index=0&lang=css& ***!
+  \*************************************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
@@ -121,9 +122,9 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 /* 13 */
-/*!***************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************!*\
-  !*** ./node_modules/mini-css-extract-plugin/dist/loader.js??ref--6-oneOf-1-0!./node_modules/@dcloudio/vue-cli-plugin-uni/packages/webpack-preprocess-loader??ref--6-oneOf-1-1!./node_modules/css-loader/dist/cjs.js??ref--6-oneOf-1-2!./node_modules/@dcloudio/vue-cli-plugin-uni/packages/vue-loader/lib/loaders/stylePostLoader.js!./node_modules/postcss-loader/src??ref--6-oneOf-1-3!./node_modules/@dcloudio/vue-cli-plugin-uni/packages/vue-loader/lib??vue-loader-options!./node_modules/@dcloudio/webpack-uni-mp-loader/lib/style.js!C:/Users/shiun/Documents/HBuilderProjects/test/App.vue?vue&type=style&index=0&lang=css& ***!
-  \***************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************/
+/*!*****************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/mini-css-extract-plugin/dist/loader.js??ref--6-oneOf-1-0!./node_modules/@dcloudio/vue-cli-plugin-uni/packages/webpack-preprocess-loader??ref--6-oneOf-1-1!./node_modules/css-loader/dist/cjs.js??ref--6-oneOf-1-2!./node_modules/@dcloudio/vue-cli-plugin-uni/packages/vue-loader/lib/loaders/stylePostLoader.js!./node_modules/postcss-loader/src??ref--6-oneOf-1-3!./node_modules/@dcloudio/vue-cli-plugin-uni/packages/vue-loader/lib??vue-loader-options!./node_modules/@dcloudio/webpack-uni-mp-loader/lib/style.js!C:/Users/徐紫云/Documents/HBuilderProjects/WeChat_YTH_MaskStore/App.vue?vue&type=style&index=0&lang=css& ***!
+  \*****************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
